9b16f4ba8da9bb96fea479f0c3baa6ba09cef740
Dzmitry Matus <dzmitry_matus@epam.com>
2022-02-10 23:11:00 +0300

    Add email validator

--- courses-app/src/app/shared/shared.module.ts
+++ courses-app/src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as Components from './components/index';
 import * as Pipes from './pipes/index';
+import * as Validators from './validators/index';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const componentsArray: Array<any> = [
   Components.ButtonComponent,
@@ -15,16 +17,25 @@ const componentsArray: Array<any> = [
   Components.InfoComponent,
   Components.SearchComponent,
   Components.ConfirmModalWindowComponent,
-  Pipes.JoinPipe
+  Pipes.JoinPipe,
+  Validators.EmailValidatorDirective
 ];
 
 @NgModule({
   declarations: componentsArray,
   imports: [
     CommonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  exports: [...componentsArray, CommonModule, FontAwesomeModule]
+  exports: [
+    ...componentsArray,
+    CommonModule,
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
 })
 export class SharedModule {
   constructor(library: FaIconLibrary) {
---
+++ courses-app/src/app/shared/validators/email-validator/email-validator.directive.ts
@@ -0,0 +1,23 @@
+import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+
+export function validateEmail(control: AbstractControl): ValidationErrors | null {
+  var regExp = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$');
+  
+  return regExp.test(control.value) ? null : {emailValidator : true}; 
+}
+
+@Directive({
+  selector: '[emailValidator]',
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: EmailValidatorDirective,
+    multi: true}]
+})
+export class EmailValidatorDirective implements Validator {
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    return validateEmail(control);
+  }
+
+}
