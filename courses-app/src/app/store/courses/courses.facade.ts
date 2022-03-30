42036f434c88a8bd18d97ec23df20e276e17171a
Dzmitry Matus <dzmitry_matus@epam.com>
2022-03-30 12:13:04 +0300

    [module5] add courses facade

--- courses-app/src/app/store/courses/courses.facade.ts
+++ courses-app/src/app/store/courses/courses.facade.ts
@@ -9,11 +9,38 @@ import * as CoursesSelectors from "./courses.selectors";
   providedIn: 'root',
 })
 export class CoursesStateFacade {
+
+  isAllCoursesLoading$ = this.store.pipe(select(CoursesSelectors.isAllCoursesLoadingSelector));
+  isSingleCourseLoading$ = this.store.pipe(select(CoursesSelectors.isSingleCourseLoadingSelector));
+  isSearchingState$ = this.store.pipe(select(CoursesSelectors.isSearchingStateSelector));
   allCourses$ = this.store.pipe(select(CoursesSelectors.getAllCourses));
+  courses$ = this.store.pipe(select(CoursesSelectors.getCourses));
+  course$ = this.store.pipe(select(CoursesSelectors.getCourse));
+  errorMessage$ = this.store.pipe(select(CoursesSelectors.getErrorMessage));
 
   constructor(private store: Store<CoursesState>) {}
 
   getAllCourses() {
     this.store.dispatch(CoursesActions.requestAllCourses());
+  }
+
+  getFilteredCourses(filter: string) {
+    this.store.dispatch(CoursesActions.requestFilteredCourses({filter}));
+  }
+
+  createCourse(course: CourseStoreModel) {
+    this.store.dispatch(CoursesActions.requestCreateCourse({course}));
+  }
+
+  getSingleCourse(id: string) {
+    this.store.dispatch(CoursesActions.requestSingleCourse({courseId: id}));
+  }
+
+  editCourse(course: CourseStoreModel) {
+    this.store.dispatch(CoursesActions.requestEditCourse({course}));
+  }
+
+  deleteCourse(courseId: string) {
+    this.store.dispatch(CoursesActions.requestDeleteCourse({courseId}));
   }
 }
